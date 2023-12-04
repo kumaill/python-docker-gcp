66e95d2cd75b426950eadf7e96f3318bcb52fe05
Kumail <lakhanikumaill@gmail.com>
2023-12-04 15:44:05 +1100

    Updated cloudbuild value to work with cloud functions

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-from flask import Flask
-app = Flask(__name__)
+# from flask import Flask
+# app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, Docker using new flask app on Cloud Function!!'
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, Docker using new flask app on Cloud Function!!'
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+import functions_framework
+
+# Register an HTTP function with the Functions Framework
+@functions_framework.http
+def my_http_function(request):
+  print("Hello World")
+  # Return an HTTP response
+  return 'OK'
