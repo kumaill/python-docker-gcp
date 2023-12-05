f3ceda922464f7c8de5cf34762c8e4ded21f0dbf
Kumail <lakhanikumaill@gmail.com>
2023-12-06 09:28:17 +1100

    develop1

---
+++ deploy1/main.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, Docker using new flask app on Cloud Function1'
+
+if __name__ == "__main__":
+    app.run(debug=True)
