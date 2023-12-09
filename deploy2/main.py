778c69bfd4fb40abb08922b372ead745aabdd7b8
Kumail <lakhanikumaill@gmail.com>
2023-12-09 12:25:40 +1100

    Updated cloudbuild value to work with cloud functions2.1

--- deploy2/main.py
+++ deploy2/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Docker using new flask app on Cloud Function2'
+    return 'Hello, Docker using new flask app on Cloud Function2.1'
 
 if __name__ == "__main__":
     app.run(debug=True)
